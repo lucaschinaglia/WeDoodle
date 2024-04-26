3adcade0bac09bcb76a7764e868193051a1e6772
lucaschinaglia <lucas.chinaglia00@gmail.com>
2024-04-26 19:30:07 -0300

    Initial Commit

---
+++ WeDoodle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeDoodle
+//
+//  Created by Lucas Chinaglia de Oliveira on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeDoodle/WeDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeDoodleApp.swift
+//  WeDoodle
+//
+//  Created by Lucas Chinaglia de Oliveira on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
